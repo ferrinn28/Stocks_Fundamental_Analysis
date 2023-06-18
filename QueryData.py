04daf52d98643b2c942d0006c8eead55f828cfbb
ferrinn28 <ferrinnmarkoni@gmail.com>
2023-06-18 15:09:56 +0700

    Adding Query for Income Statement

--- QueryData.py
+++ QueryData.py
@@ -17,6 +17,12 @@ class Data:
     def get_balance_sheet_annual(self):
         return(self.query.balance_sheet("a"))
     
+    def get_income_statement_quarter(self):
+        return(self.query.income_statement("q"))
+    
+    def get_income_statement_annual(self):
+        return(self.query.income_statement("a"))
+    
     def get_history_price(self, period="ytd", interval="1d", start=None, end=None):
         return(self.query.history(period=period, interval=interval, start=start, end=end))
     
