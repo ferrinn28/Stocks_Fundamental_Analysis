3604875121b3e9b78d30c945807ae6675132c6b5
ferrinn28 <ferrinnmarkoni@gmail.com>
2023-07-15 14:51:51 +0700

    Adding Feature for Annual Calculation

--- QueryData.py
+++ QueryData.py
@@ -21,8 +21,10 @@ class Data:
     
     def get_history_price(self, period="ytd", interval="1d", start=None, end=None):
         return(self.query.history(period=period, interval=interval, start=start, end=end))
-    
-#Sub Class
+
+
+
+#Sub Class Quarter Data Value
 class CalculateQuarter(Data):
     def __init__(self, ticker, date_input):
         super().__init__(ticker)
@@ -115,6 +117,101 @@ class CalculateQuarter(Data):
         cumulative_net_income = self.cumulative_net_income
         total_quartal = self.quartal
         eps = ((cumulative_net_income * (4/total_quartal))/(self.shares))
+
+        return float(f'{eps:.2f}')
+    
+    def calculate_PER(self):
+        #Get Historical Price
+        historical_price = self.get_specific_historical_price()
+
+        #Calculate Price Earning Ratio
+        per = historical_price/(self.calculate_EPS())
+
+        return float(f'{per:.2f}')
+
+
+
+#Sub Class Annual Data Value
+class CalculateAnnual(Data):
+    def __init__(self, ticker, date_input):
+        super().__init__(ticker)
+        self.balance_sheet_annual = self.get_balance_sheet_annual()
+        self.income_statement_annual = self.get_income_statement_annual()
+        self.input_date = pd.to_datetime(date_input)
+
+        #Gets Date Component (Year, Month, Date)
+        self.year = self.input_date.year
+        self.month = self.input_date.month
+        self.day = self.input_date.day
+
+        #Always Used Components
+        ##Select Row Based on User Date Input
+        rows = self.balance_sheet_annual[self.balance_sheet_annual['asOfDate'] == self.input_date]
+        self.equity = rows["StockholdersEquity"][0]
+        self.shares = rows["ShareIssued"][0]
+
+        ##Collect 1 Month Historical Price
+        self.historical_price = self.get_history_price(period="1mo", interval="1d", 
+                                                       start=f"{self.year}-{self.month}-1", end=f"{self.year}-{self.month}-{self.day}")
+        
+        ##Select Some Annual in a Year based on user input date
+        selected_income_statement = self.income_statement_annual[(self.income_statement_annual['asOfDate'].dt.year == self.year) 
+                                                                 & (self.income_statement_annual['periodType'] == "12M")]
+        
+        ###Revenue in period of time
+        self.revenue = selected_income_statement["TotalRevenue"][0]
+
+        #Net Income in period of time
+        self.net_income = selected_income_statement["NetIncomeCommonStockholders"][0]
+        
+    def get_specific_historical_price(self):
+        #Change Date Index into New Column
+        historical_price = self.historical_price.reset_index()
+        historical_price["date"] = pd.to_datetime(historical_price["date"])
+        
+        #Check If On a date has a value
+        selected_history = historical_price[historical_price["date"] == self.input_date]
+        if selected_history.empty:
+            i = 1
+            while selected_history.empty:
+                selected_history = historical_price[historical_price["date"].dt.day == self.day -i]
+                i = i + 1
+
+            return selected_history.iloc[0]["adjclose"]
+        else:
+
+            return selected_history.iloc[0]["adjclose"]
+
+    def calculate_book_value(self):  
+        # Calculate Book Value
+        bv = (self.equity/self.shares)
+
+        return int(bv)
+    
+    def calculate_price_book_value(self):
+        #Get Historical Price
+        historical_price = self.get_specific_historical_price()
+
+        #Calculate Price Book Ratio
+        pbv = historical_price/(self.calculate_book_value())
+
+        return float(f'{pbv:.2f}')
+
+    def calculate_ROE(self):
+        #ROE Data
+        roe = ((self.net_income)/(self.equity))*100
+
+        return float(f'{roe:.2f}')
+    
+    def calculate_net_profit_margin(self):
+        #NPM Data
+        npm = (self.net_income/self.revenue)*100
+
+        return float(f'{npm:.2f}')
+
+    def calculate_EPS(self):
+        #EPS Data
+        eps = ((self.net_income)/(self.shares))
 
         return float(f'{eps:.2f}')
     
--- main.py
+++ main.py
@@ -1,48 +1,94 @@
-from QueryData import Data, CalculateQuarter
+from QueryData import Data, CalculateQuarter, CalculateAnnual
 import json
 import pandas as pd
 
 if __name__ == "__main__":
     ticker_code = input("Kode Emiten: ")
-
     sheet_overview = Data(ticker_code)
-    list_date = sheet_overview.get_balance_sheet_quarter()['asOfDate'].dt.strftime('%Y-%m-%d').tolist()
-    print("AVAILABLE DATE FOR THE REPORTS")
-    for i, j in enumerate(list_date, start=1):
-        print(f"{i}.", j)
+    checking = input("Check Annual (a) or Quartal (q)?:")
 
-    #Choose Date to Review
-    date_input = int(input("Choose the Date: "))
-    date = list_date[date_input -1]
+    if checking.lower() == "q":
+        list_date = sheet_overview.get_balance_sheet_quarter()['asOfDate'].dt.strftime('%Y-%m-%d').tolist()
+        print("AVAILABLE DATE FOR THE REPORTS")
+        for i, j in enumerate(list_date, start=1):
+            print(f"{i}.", j)
 
-    #Calculating Fundamental Parameter
-    searching_data = CalculateQuarter(ticker_code, date)
+        #Choose Date to Review
+        date_input = int(input("Choose the Date: "))
+        date = list_date[date_input -1]
 
-    #Output in Dictionary
-    pd.options.display.float_format = '{:.2f}'.format
+        #Calculating Fundamental Parameter
+        searching_data = CalculateQuarter(ticker_code, date)
 
-    data_fundamentals = {
-        "Code": f"{ticker_code}",
-        "Date": date,
-        "Fundamental Data": {
-            "Cumulative Revenue": searching_data.cumulative_revenue,        #in rupiah
-            "Cumulative Net Income": searching_data.cumulative_net_income,  #in rupiah
-            "BV": searching_data.calculate_book_value(),                    #in rupiah
-            "PBV": searching_data.calculate_price_book_value(),
-            "NPM": searching_data.calculate_net_profit_margin(),            #in %
-            "ROE": searching_data.calculate_ROE(),                          #in %
-            "EPS": searching_data.calculate_EPS(),                          #in rupiah
-            "PER": searching_data.calculate_PER()
+        #Output in Dictionary
+        pd.options.display.float_format = '{:.2f}'.format
+
+        data_fundamentals = {
+            "Code": f"{ticker_code}",
+            "Type Report": f"{checking}",
+            "Date": date,
+            "Fundamental Data": {
+                "Cumulative Revenue": searching_data.cumulative_revenue,        #in rupiah
+                "Cumulative Net Income": searching_data.cumulative_net_income,  #in rupiah
+                "BV": searching_data.calculate_book_value(),                    #in rupiah
+                "PBV": searching_data.calculate_price_book_value(),
+                "NPM": searching_data.calculate_net_profit_margin(),            #in %
+                "ROE": searching_data.calculate_ROE(),                          #in %
+                "EPS": searching_data.calculate_EPS(),                          #in rupiah
+                "PER": searching_data.calculate_PER()
+            }
         }
-    }
 
-    #Convert into json format
-    json_format = json.dumps(data_fundamentals, indent=4)
-    print(json_format, "\n")
+        #Convert into json format
+        json_format = json.dumps(data_fundamentals, indent=4)
+        print(json_format, "\n")
 
-    #Convert into Dataframe
-    df_upload = pd.DataFrame(json.loads(json_format))
-    print(df_upload)
-    #print(df_upload.index.values)
-    #print(df_upload.loc["BV"]["Code"])
-    #df_upload.to_excel(excel_writer="C:\\Users\\ASUS\\Documents\\FInancial Startegy\\Project_Fundamental\\Testing\\output.xlsx", sheet_name=f'{ticker_code}')
+        #Convert into Dataframe
+        df_upload = pd.DataFrame(json.loads(json_format))
+        print(df_upload)
+        #print(df_upload.index.values)
+        #print(df_upload.loc["BV"]["Code"])
+        #df_upload.to_excel(excel_writer="C:\\Users\\ASUS\\Documents\\FInancial Startegy\\Project_Fundamental\\Testing\\output.xlsx", sheet_name=f'{ticker_code}')
+
+    elif checking.lower() =="a":
+        list_date = sheet_overview.get_balance_sheet_annual()['asOfDate'].dt.strftime('%Y-%m-%d').tolist()
+        print("AVAILABLE DATE FOR THE REPORTS")
+        for i, j in enumerate(list_date, start=1):
+            print(f"{i}.", j)
+
+        #Choose Date to Review
+        date_input = int(input("Choose the Date: "))
+        date = list_date[date_input -1]
+
+        #Calculating Fundamental Parameter
+        searching_data = CalculateAnnual(ticker_code, date)
+
+        #Output in Dictionary
+        pd.options.display.float_format = '{:.2f}'.format
+
+        data_fundamentals = {
+            "Code": f"{ticker_code}",
+            "Type Report": f"{checking}",
+            "Date": date,
+            "Fundamental Data": {
+                "Cumulative Revenue": searching_data.revenue,                   #in rupiah
+                "Cumulative Net Income": searching_data.net_income,             #in rupiah
+                "BV": searching_data.calculate_book_value(),                    #in rupiah
+                "PBV": searching_data.calculate_price_book_value(),
+                "NPM": searching_data.calculate_net_profit_margin(),            #in %
+                "ROE": searching_data.calculate_ROE(),                          #in %
+                "EPS": searching_data.calculate_EPS(),                          #in rupiah
+                "PER": searching_data.calculate_PER()
+            }
+        }
+
+        #Convert into json format
+        json_format = json.dumps(data_fundamentals, indent=4)
+        print(json_format, "\n")
+
+        #Convert into Dataframe
+        df_upload = pd.DataFrame(json.loads(json_format))
+        print(df_upload)
+        #print(df_upload.index.values)
+        #print(df_upload.loc["BV"]["Code"])
+        #df_upload.to_excel(excel_writer="C:\\Users\\ASUS\\Documents\\FInancial Startegy\\Project_Fundamental\\Testing\\output.xlsx", sheet_name=f'{ticker_code}')
