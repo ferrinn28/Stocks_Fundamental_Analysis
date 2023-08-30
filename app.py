4e7f1636c43d8d3f3f8ef8b622633161e71d990d
ferrinn28 <ferrinnmarkoni@gmail.com>
2023-08-30 21:00:31 +0700

    Testing routing and connection calculate_quarter module

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+from analysis_data import CalculateQuarter
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Welcome!</p>"
+
+@app.route("/api/bmri")
+def query():
+    data = CalculateQuarter("BMRI", "2023-03-31").output()
+    pretty_json = json.dumps(data, indent=4)
+    return '<pre>{}</pre>'.format(pretty_json)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
