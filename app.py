14588aedd1fa28fa8cb40cafc981cb264e833656
ferrinn28 <ferrinnmarkoni@gmail.com>
2023-08-31 19:46:32 +0700

    Create Endpoint for querying basic info of company

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from analysis_data import CalculateQuarter
+from analysis_data import CalculateQuarter, QueryData
 import json
 
 app = Flask(__name__)
@@ -8,11 +8,10 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Welcome!</p>"
 
-@app.route("/api/bmri")
-def query():
-    data = CalculateQuarter("BMRI", "2023-03-31").output()
-    pretty_json = json.dumps(data, indent=4)
-    return '<pre>{}</pre>'.format(pretty_json)
+@app.route("/api/<ticker>")
+def query_basic_info(ticker):
+    data = QueryData(str(ticker.upper())).get_basic_info()
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
